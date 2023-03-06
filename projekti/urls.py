e02e0b51c62d6f04f9c373e4a2269c8bb5111548
TimoKuukka <46355@edu.raseko.fi>
2023-03-06 11:00:23 +0200

    Aseta mediapolku

--- projekti/urls.py
+++ projekti/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
 from sovellus import views as blogi_views
 
 urlpatterns = [
@@ -23,4 +24,4 @@ urlpatterns = [
     path('', blogi_views.postaukset, name="postauslista"),
     path('postaus/<int:id>', blogi_views.nayta_postaus, name="nayta_postaus"),
     path('uusi/', blogi_views.uusi_postaus, name="uusi_postaus"),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
