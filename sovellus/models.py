3a29bddc09796b19fc3a514d5e371eebc3c5069d
TimoKuukka <46355@edu.raseko.fi>
2023-03-12 21:21:12 +0200

    Salli tyhjä kuva Django Adminissa

--- sovellus/models.py
+++ sovellus/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. Aina kun teet täällä muutoksia niin luo uusi migraatio komennoilla:
+# python manage.py makemigrations
+# python manage.py migrate
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
-    kuva = models.ImageField(upload_to="blogi/kuvat", null=True)
+    kuva = models.ImageField(upload_to="blogi/kuvat", null=True, blank=True) #null=True = tietokanta voi olla ilman kuvaa, djnago admin tarvitsee lisäksi että kenttä saa jäädä tyhjäksi -> blank=True
     luotu = models.DateTimeField(auto_now_add = True) #pvm ja ajan tallentamista, milloin luotu.
 
     def __str__(self):
